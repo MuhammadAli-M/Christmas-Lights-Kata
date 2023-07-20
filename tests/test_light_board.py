e8cd32fb542b2dfb22c8dc8f9fd2a1432e681d8e
Muhammad Adam <muhammadalyadam@gmail.com>
2023-07-20 12:53:00 +0300

    Add test, turn on square of length 2

--- tests/test_light_board.py
+++ tests/test_light_board.py
@@ -7,6 +7,11 @@ class Board:
         self.grid = list(
             map(lambda x: list(map(lambda y: 0, range(number))), range(number)))
 
+    def turn_on(self, first_cord, second_cord):
+        for row in range(first_cord[0], second_cord[0] + 1):
+            for col in range(first_cord[1], second_cord[1] + 1):
+                self.grid[row][col] = 1
+
 
 class TestLightBoard(TestCase):
 
@@ -25,3 +30,10 @@ class TestLightBoard(TestCase):
         for row in range(len(board.grid)):
             for col in board.grid[row]:
                 self.assertEqual(board.grid[row][col], 0)
+
+    def test_board_turn_on_block_of_10_from_0_0_to_1_1(self):
+        board = Board(10)
+
+        board.turn_on((0, 0), (1, 1))
+        actual = list(map(lambda row: row[0:2], board.grid[0:2]))
+        self.assertEqual(actual, [[1, 1], [1, 1]])
