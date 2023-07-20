f458263ae7b02770a6b92c351768de619bd9aa02
Muhammad Adam <muhammadalyadam@gmail.com>
2023-07-20 20:15:21 +0300

    Refactor board and clean unused code

--- app/board.py
+++ app/board.py
@@ -12,6 +12,13 @@ class Board:
         self.lighted += (self.grid[row][col] + 1) % 2
         self.grid[row][col] = 1
 
+    def turn_off(self, first_cord, second_cord):
+        self._apply_on_block(first_cord, second_cord, self.perform_turn_off)
+
+    def perform_turn_off(self, row, col):
+        self.lighted -= (self.grid[row][col]) % 2
+        self.grid[row][col] = 0
+
     def toggle(self, first_cord, second_cord):
         self._apply_on_block(first_cord, second_cord, self.perform_toggle)
 
@@ -19,18 +26,7 @@ class Board:
         self.lighted -= ((self.grid[row][col]) * 2) - 1
         self.grid[row][col] = int(not self.grid[row][col])
 
-    def turn_off(self, first_cord, second_cord):
-        self._apply_on_block(first_cord, second_cord, self.perform_turn_off)
 
-    @staticmethod
-    def get_cells_affected_count(first_cord, second_cord):
-        diff_in_x = second_cord[0] - first_cord[0] + 1
-        diff_in_y = second_cord[1] - first_cord[1] + 1
-        return diff_in_x * diff_in_y
-
-    def perform_turn_off(self, row, col):
-        self.lighted -= (self.grid[row][col]) % 2
-        self.grid[row][col] = 0
 
     @staticmethod
     def _apply_on_block(first_cord, second_cord, method):
