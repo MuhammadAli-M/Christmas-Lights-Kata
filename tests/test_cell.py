966b09033abb14156da9b5f56f8a535a2c91d2d6
Muhammad Adam <muhammadalyadam@gmail.com>
2023-07-21 00:20:20 +0300

    Add cell turn_on

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -2,9 +2,16 @@ from unittest import TestCase
 
 
 class LightCell:
-    def __init__(self, x, y):
+    DEFAULT_VALUE = 0
+    TURNED_ON_VALUE = 1
+
+    def __init__(self, x, y, value=DEFAULT_VALUE):
+        self.value = value
         self.x = x
         self.y = y
+
+    def turn_on(self):
+        self.value = self.TURNED_ON_VALUE
 
 
 class TestLightCell(TestCase):
@@ -14,3 +21,8 @@ class TestLightCell(TestCase):
 
         self.assertEqual(cell.x, 0)
         self.assertEqual(cell.y, 0)
+
+    def test_cell_turn_on_works(self):
+        cell = LightCell(0, 0)
+        cell.turn_on()
+        self.assertEqual(cell.value, 1)
