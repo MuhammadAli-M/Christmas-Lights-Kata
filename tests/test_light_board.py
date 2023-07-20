5757da46f4ee2c656a4d4fbcfd1a1958c983c30f
Muhammad Adam <muhammadalyadam@gmail.com>
2023-07-20 12:25:09 +0300

    Add test, init with 500

--- tests/test_light_board.py
+++ tests/test_light_board.py
@@ -5,15 +5,23 @@ class Board:
 
     def __init__(self, number):
         self.grid = list(
-            map(lambda x: list(map(lambda y: 0, range(1000))), range(1000)))
+            map(lambda x: list(map(lambda y: 0, range(number))), range(number)))
 
 
 class TestLightBoard(TestCase):
 
-    def test_board_init(self):
+    def test_board_init_1000(self):
         board = Board(1000)
         self.assertEqual(len(board.grid[0]), 1000)
         self.assertEqual(len(board.grid[999]), 1000)
+        for row in range(len(board.grid)):
+            for col in board.grid[row]:
+                self.assertEqual(board.grid[row][col], 0)
+
+    def test_board_init_500(self):
+        board = Board(500)
+        self.assertEqual(len(board.grid[0]), 500)
+        self.assertEqual(len(board.grid[499]), 500)
         for row in range(len(board.grid)):
             for col in board.grid[row]:
                 self.assertEqual(board.grid[row][col], 0)
