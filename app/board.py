76c282e641b320b52607c55cb9d0a71b67e420c5
Muhammad Adam <muhammadalyadam@gmail.com>
2023-07-20 14:41:02 +0300

    Refactor applying an action on block

--- app/board.py
+++ app/board.py
@@ -5,6 +5,13 @@ class Board:
             map(lambda x: list(map(lambda y: 0, range(number))), range(number)))
 
     def turn_on(self, first_cord, second_cord):
+        self._apply_on_block(first_cord, second_cord, self._perform_turn_on)
+
+    def _perform_turn_on(self, col, row):
+        self.grid[row][col] = 1
+
+    @staticmethod
+    def _apply_on_block(first_cord, second_cord, method):
         for row in range(first_cord[0], second_cord[0] + 1):
             for col in range(first_cord[1], second_cord[1] + 1):
-                self.grid[row][col] = 1
+                method(row, col)
