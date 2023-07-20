f7d6cd06e3f26adad68736d1b3d21ff21e650396
Muhammad Adam <muhammadalyadam@gmail.com>
2023-07-20 18:07:34 +0300

    Clean the board

--- app/board.py
+++ app/board.py
@@ -16,14 +16,14 @@ class Board:
     def perform_toggle(self, row, col):
         self.grid[row][col] = int(not self.grid[row][col])
 
-    @staticmethod
-    def _apply_on_block(first_cord, second_cord, method):
-        for row in range(first_cord[0], second_cord[0] + 1):
-            for col in range(first_cord[1], second_cord[1] + 1):
-                method(row, col)
-
     def turn_off(self, first_cord, second_cord):
         self._apply_on_block(first_cord, second_cord, self.perform_toggle)
 
     def perform_turn_off(self, row, col):
         self.grid[row][col] = 0
+
+    @staticmethod
+    def _apply_on_block(first_cord, second_cord, method):
+        for row in range(first_cord[0], second_cord[0] + 1):
+            for col in range(first_cord[1], second_cord[1] + 1):
+                method(row, col)
